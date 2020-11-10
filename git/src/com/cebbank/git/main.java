66329349298a70f0c58031232dbfc50e7f843040
xuzhi <xuzhichn@163.com>
2020-11-10 15:35:33 +0800

    third edit

--- git/src/com/cebbank/git/main.java
+++ git/src/com/cebbank/git/main.java
@@ -5,6 +5,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("this is git test");
         System.out.println("this is second edit");
+        System.out.println("this is third update");
     }
 
     public void t1(){
